3d5b7f3e64e3650593058e583d2470fc5a49dc16
LaiaFont <74786722+LaiaFont@users.noreply.github.com>
2021-12-14 19:15:36 +0100

    Afegits seeder paisos

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,5 +14,45 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('pais')->insert([
+            'nom'=>'Espanya',
+            'continent'=>'Europa'
+        ]);
+        DB::table('pais')->insert([
+            'nom'=>'Itàlia',
+            'continent'=>'Europa'
+        ]);
+        DB::table('pais')->insert([
+            'nom'=>'Xina',
+            'continent'=>'Àsia'
+        ]);
+        DB::table('pais')->insert([
+            'nom'=>'Tailàndia',
+            'continent'=>'Àsia'
+        ]);
+        DB::table('pais')->insert([
+            'nom'=>'Sydney',
+            'continent'=>'Austràlia'
+        ]);
+        DB::table('pais')->insert([
+            'nom'=>'Rússia',
+            'continent'=>'Àsia'
+        ]);
+        DB::table('pais')->insert([
+            'nom'=>'Mèxic',
+            'continent'=>'Amèrica'
+        ]);
+        DB::table('pais')->insert([
+            'nom'=>'Argentina',
+            'continent'=>'Amèrica'
+        ]);
+        DB::table('pais')->insert([
+            'nom'=>'Egipte',
+            'continent'=>'Àfrica'
+        ]);
+        DB::table('pais')->insert([
+            'nom'=>'Senegal',
+            'continent'=>'Àfrica'
+        ]);
     }
 }
