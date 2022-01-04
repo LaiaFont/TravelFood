256c223cbb81282dbb4ff3e27ec7028c7ce8897b
LaiaFont <laia.font2@uvic.cat>
2022-01-04 12:01:56 +0100

    Canvis en estil

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Continents</a>
+        <a class="navbar-brand">Continents</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDarkDropdown" aria-controls="navbarNavDarkDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -8,7 +8,7 @@
             <ul class="navbar-nav">
                 @foreach ($continents as $continent) 
                 <li class="nav-item dropdown">
-                    <a class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-bs-toggle="dropdown">
+                    <a class="btn btn-lg btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" data-bs-toggle="dropdown">
                         {{ $continent }}
                     </a>
                     <ul class = "dropdown-menu" aria-labelledby="dropdownMenuButton">
