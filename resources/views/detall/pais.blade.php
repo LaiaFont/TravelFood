87774b46348ee2731c5094d30cb94fcb16a322e6
LaiaFont <laia.font2@uvic.cat>
2022-01-04 19:35:36 +0100

    Header i footer

--- resources/views/detall/pais.blade.php
+++ resources/views/detall/pais.blade.php
@@ -1,5 +1,15 @@
+@include('includes.page_head')
+
+@include('layouts.header')
+
+@section('content')
+
 @foreach ($ciutats as $ciutat)
     @if ($ciutat->pais_id == $id)
         {{ $ciutat->ciutat_nom }}
     @endif
 @endforeach
+
+
+@include('layouts.footer')
+@include('includes.page_tail')
