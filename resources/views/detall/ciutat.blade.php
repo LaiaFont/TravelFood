7cd8995c2b0f2e336bd2ba485cac56f647c7149b
LaiaFont <laia.font2@uvic.cat>
2022-01-08 13:04:51 +0100

    Ciutat-Plat afegit

---
+++ resources/views/detall/ciutat.blade.php
@@ -0,0 +1,21 @@
+@include('includes.page_head')
+
+@include('layouts.header')
+@include('layouts.menu')
+
+@section('content')
+<div class="card-group mt-3">
+    @foreach ($ciutats as $ciutat)
+        <div class="card" style="width: 18rem;">
+            <a href="{{ $ciutat->pais_id }}/{{ $ciutat->id }}">
+                <img class="card-img" src="{{ $ciutat->imatge_url }}" alt="Card image">
+                <div class="card-body">
+                    <h5 class="card-title">{{ $ciutat->ciutat_nom }}</h5>
+                </div>
+            </a>
+        </div>
+    @endforeach
+</div>
+
+@include('layouts.footer')
+@include('includes.page_tail')
