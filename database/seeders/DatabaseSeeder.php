d9b521f324c525f398b710c0cf29e9a485ab1da9
AlbertsUVic <albert.vargas@uvic.cat>
2022-01-11 20:22:23 +0100

    Afegides fotos plats

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -261,18 +261,18 @@ class DatabaseSeeder extends Seeder
         //Italia
         DB::table('plat')->insert([
             'nom'=> 'Filet Florentí',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://italian-traditions.com/wp-content/uploads/2018/07/fiorentina-2.jpg',
             'descripcio'=> "Es prepara ruixant tots dos costats de la carn amb unes gotes d'oli d'oliva. Després es cuina a la graella i es rosteix a les brases fins a la cocció desitjada."
         ]);
         DB::table('plat')->insert([
             'nom'=> 'Pizza Napolitana',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://upload.wikimedia.org/wikipedia/commons/a/a3/Eq_it-na_pizza-margherita_sep2005_sml.jpg',
             'descripcio'=> "No es pot discutir que la pizza napolitana és la millor d'Itàlia. Les seves dues especialitats són la margarida, amb tomàquet, alfàbrega i oli; i la marinera, amb oli d'oliva, tomàquet, all i orenga. A més, la massa de les pizzes napolitanes és més fina."
         ]);
 
         DB::table('plat')->insert([
             'nom'=> 'Crostini di Fegato',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://assets.tmecosys.com/image/upload/t_web767x639/img/recipe/vimdb/239104.jpg',
             'descripcio'=> "Un antipasto florentí molt típic són els crostini di fegato, que consisteix en pa fregit coberts en una barreja de fetge (vedella, pollastre, oca, ànec…) barrejades amb anxoves picades, ceba i tàperes."
         ]);
 
@@ -280,150 +280,150 @@ class DatabaseSeeder extends Seeder
 
         DB::table('plat')->insert([
             'nom'=> 'Cerdo asado rojo',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://us.123rf.com/450wm/watthano/watthano1409/watthano140900006/31424782-cerdo-rojo-asado-con-salsa-la-cocina-china.jpg?ver=6',
             'descripcio'=> "El color característic d'aquest plat és donat per una salsa de soja amb sucre i una pasta de mongetes amb què es cuina el porc. Pots trobar aquest menjar típic tant en parades de carrer com als millors restaurants de Xangai."
         ]);
 
         DB::table('plat')->insert([
             'nom'=> 'Hot Pot',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://www.cocinista.es/download/bancorecursos/recetas/receta-olla-china-hotpot.jpg',
             'descripcio'=> "El seu nom fa referència al mode de preparació i com se serveix aquest menjar típic: en una olla calenta i al centre de la taula. Cada comensal fica els seus trossos de carn, peix i verdura a l'olla perquè es vagi fent."
         ]);
 
         DB::table('plat')->insert([
             'nom'=> 'Empanades Goubuli',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://i.pinimg.com/originals/5e/ec/54/5eec54c2f4163de7ee98e75e99eee442.jpg',
             'descripcio'=> "És la marca tradicional i representativa dels entrepans de Tianjin. A més dels sabors tradicionals, Goubuli ha afegit variants com les empanades de Perla, de Drac i Fènix, les empanades de marisc, entre d'altres."
         ]);
 
         //Tailandia
         DB::table('plat')->insert([
             'nom'=> 'Pad Thai',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://i.blogs.es/f9e85e/receta-de-pad-thai-pakus-directo-paladar/1366_2000.jpg',
             'descripcio'=> "El menjar típic tailandès per excel·lència. No pots visitar Bangkok o qualsevol ciutat de Tailàndia i anar-te'n sense tastar aquest plat típic. Els noodles van acompanyats de soja, vegetals o potser una mica de carn i fruits secs picats."
         ]);
 
         DB::table('plat')->insert([
             'nom'=> 'Khao Pad Saparod',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://i1.wp.com/elcalderoviajero.com/wp-content/uploads/2017/12/arroz-frito-con-pi%C3%B1a-tailandes-10.jpg?fit=750%2C498&ssl=1',
             'descripcio'=> "A més del Pad Thai, el Khao Pad Saparod és el típic plat que us agradarà a gairebé tots. Es tracta d'arròs fregit amb gambes o pollastre, pinya, ou, anacards, panses i salsa de peix. Es fa amb curri en pols així que també té un toc de gust de curri."
         ]);
 
         DB::table('plat')->insert([
             'nom'=> 'Pad Siuw',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://www.196flavors.com/wp-content/uploads/2017/05/pad-see-ew-6-FP.jpg',
             'descripcio'=> "És un plat de fideus saltejats amb pollastre, ou, pebre, all i fet amb salsa d'ostres. Els fideus són més grans i enganxosos que els del Pad Thai de manera que no és un plat que sol tenir tant èxit. La textura dels fideus pot semblar estranya al principi."
         ]);
 
         //Australia
         DB::table('plat')->insert([
             'nom'=> 'Cangur a la Parrilla',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://comidatipica.blog/wp-content/uploads/2020/10/Canguro-a-la-parrilla.png',
             'descripcio'=> "Encara que a molts altres països la carn de cangur és desconeguda, a Austràlia és un menjar típic que és gairebé obligatori tastar. Si tagrada experimentar aquesta carn tagradarà. Si en vols més, pots demanar carn de cocodril."
         ]);
 
         DB::table('plat')->insert([
             'nom'=> 'Yabbies',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://lacomidatipica.com/wp-content/uploads/2019/05/Yabbies-.png',
             'descripcio'=> "Encara que el seu nom sigui una mica desconegut, aquests animals són una mena de llagostes o llagostins que viuen en aigua dolça. Hi ha moltes maneres de cuinar-los i els trobaràs en més menjars típics australians."
         ]);
 
         DB::table('plat')->insert([
             'nom'=> 'Meat Pie',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://img.taste.com.au/BbERqiBQ/taste/2016/11/aussie-meat-pies-16289-1.jpeg',
             'descripcio'=> "Els pastissos de carn tradicionals d'Austràlia són de mida d'una mà i estan farcits amb carn i salsa i recoberts de salsa de tomàquet. Avui dia pots trobar pastissos amb diferents farcits i la veritat és que alguns estan boníssims."
         ]);
 
         //Russia
         DB::table('plat')->insert([
             'nom'=> 'Rassólnik',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://cdn.shopify.com/s/files/1/1257/0365/files/rassolnik1_large.jpg?v=1472748866',
             'descripcio'=> "Una cosa que és evidentment tradicional a Moscou són les sopes russes. Feta de cogombres, tubercles i carn, acompanyada de crema agra. El més característic és el suc de cogombrets en què es basa aquest plat típic rus."
         ]);
         DB::table('plat')->insert([
             'nom'=> 'Kulebiaka',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://lacomidatipica.com/wp-content/uploads/2019/04/Kulebiaka-.png',
             'descripcio'=> "Un pastís típic rus que generalment va farcit de salmó, arròs, ous durs, xampinyons o ceba. Té un aspecte de pa brioix i és una recepta que ha estat copiada per alts cuiners per experimentar amb nous sabors arreu del món."
         ]);
         DB::table('plat')->insert([
             'nom'=> 'Carn a la Stroganoff',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://www.coosur.com/uploads/recetasaceiteoliva/ternera-stroganoff.jpg',
             'descripcio'=> "La carn més típica de Rússia és la carn a la stroganoff. Són trossets de vedella, acompanyat de bolets, ceba i una forta salsa agra. El plat ve normalment acompanyat d'arròs."
         ]);
 
         //Mexic
         DB::table('plat')->insert([
             'nom'=> 'Huaraches',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://espaciomex.com/wp-content/uploads/2019/03/huarache-3323447_1920-1280x640.jpg',
             'descripcio'=> "Aquest plat típic es diu així perquè la seva base, feta de blat de moro, té la forma d'una sandàlia. A sobre s'hi posen fesols, carn de vedella i formatge, i s'acompanya d'una salsa picant."
         ]);
 
         DB::table('plat')->insert([
             'nom'=> 'Tacos Tijuana',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://tipsparatuviaje.com/wp-content/uploads/2018/04/taco-3.jpg',
             'descripcio'=> "El tac estil Tijuana té la particularitat que la truita de blat de moro té una forma de con, cosa que la fa més apta per acomodar el deliciós farcit de carn, alvocat i salsa picant de chiles."
         ]);
 
         DB::table('plat')->insert([
             'nom'=> 'Torta Ahogada',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://dam.cocinafacil.com.mx/wp-content/uploads/2017/03/torta-ahogada-.jpg',
             'descripcio'=> "Es prepara amb un boixet allargat de sabor salat partit a la meitat al que se li afegeixen carn de porc i fesols. La coca s'enfonsa en una salsa vermella picant i se serveix amb cebes llesques."
         ]);
 
         //Argentina
         DB::table('plat')->insert([
             'nom'=> 'Picada',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://i.pinimg.com/originals/66/aa/49/66aa49da3f1c0ea54a7f5fb558532d73.jpg',
             'descripcio'=> "Les picades més simples porten formatge, pernil cuit, salami, olives i patates fregits o blat de moro, però poden portar gairebé de tot: mortadel·la, truita de patates, salsitxes, falafel, anxoves…"
         ]);
 
         DB::table('plat')->insert([
             'nom'=> 'Alfajores',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://www.elmundoeats.com/wp-content/uploads/2021/07/FP2-Argentine-alfajores-on-a-rack-500x375.jpg',
             'descripcio'=> "Aquesta mena de galetes farcides són un menjar típic argentí que s'ha estès a molts altres llocs del món. Poden portar el farcit que més t'agradi (mel, fruita…) encara que els més demanats són els de dolç de llet."
         ]);
         DB::table('plat')->insert([
             'nom'=> 'Empanadas Salteñas',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://elcordillerano1.cdn.net.ar/252/elcordillerano/images/95/02/950225_b60e587383b0cb976412c5de2dc89af2532e28925576f5994b5b671b2f1d9bdb/md.webp',
             'descripcio'=> "Les empanades saltenyes es destaquen perquè contenen com a ingredient diferencial la patata i la carn es talla a ganivet. Es poden fer fregides o al forn."
         ]);
 
         //Egipte
         DB::table('plat')->insert([
             'nom'=> 'Ful Medames',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://comidasegipcias.com/wp-content/uploads/2019/09/ful-medames-receta-1024x576.jpg',
             'descripcio'=> "Un xut d'energia que has de provar almenys una vegada (aquí se sol menjar d'esmorzar). Les faves són les grans protagonistes del plat. No és estrany que per menjar-les també et serveixin pa de pita."
         ]);
 
         DB::table('plat')->insert([
             'nom'=> 'Mezze',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://tuprensalocal.com/wp-content/uploads/2021/03/2021_0307_hummus-egipto.jpg',
             'descripcio'=> "Els entrants freds o aperitius són molt populars en aquesta ciutat egípcia. Solen servir-se principalment en petits atuells o recipients."
         ]);
 
         
         DB::table('plat')->insert([
             'nom'=> 'Koftas',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://astelus.com/wp-content/viajes/la-preparacion-de-la-kofta-tipica-egipcia.jpg',
             'descripcio'=> "Aquest menjar típic del Caire i d'Egipte en general són unes boles de carn de vedella i xai amb espècies, que tradicionalment es presenten en forma de kebab."
         ]);
 
         //Senegal
         DB::table('plat')->insert([
             'nom'=> 'Maafe',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://okdiario.com/img/2018/12/04/receta-de-maafe-senegales-1.jpg',
             'descripcio'=> "Es tracta d'un dels guisats més exquisits que tastaràs, sobretot si ets amant del cacauet. És conegut com a guisat de cacauet i és popular en una gran quantitat de països, és un platet típic d'Àfrica Occidental i Àfrica Central."
         ]);
 
         DB::table('plat')->insert([
             'nom'=> 'Thieboudienne',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://www.vueltaalmundosabrosa.com/wp-content/uploads/2017/01/DSC_0430-981x640.jpg',
             'descripcio'=> "El thieboudienne és un plat de tipus been cin, on tots els ingredients es preparen en una mateixa marmita. El peix pot ser de diferents tipus, juntament amb l'arròs i el tomàquet, els ingredients poden variar d'un lloc a un altre."
         ]);
 
         DB::table('plat')->insert([
             'nom'=> 'Fataya',
-            'imatge_url'=> 'https://cdn02.visitbarcelona.com/files/11116-3-imagen/pa-tomaquet-recepta-gastronomia-catalunya-enjoy-c1b.jpg',
+            'imatge_url'=> 'https://media-cdn.tripadvisor.com/media/photo-s/19/1f/8b/10/fataya-senegal-food.jpg',
             'descripcio'=> "Són crestes de carn o peix amb ceba, tomàquet i pebre, mateixes que són acompanyades d'una salsa picant per “remullar-se”."
         ]);
 
