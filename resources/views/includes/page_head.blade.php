59bbf69b657ab6fdd24e56c7b7255f159d469c05
LaiaFont <laia.font2@uvic.cat>
2022-01-16 16:39:11 +0100

    Canvis

--- resources/views/includes/page_head.blade.php
+++ resources/views/includes/page_head.blade.php
@@ -8,12 +8,11 @@
   <!-- CSRF Token -->
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>{{ config('app.name', 'Laravel') }}</title>
+  <title>{{ config('app.name', 'Travel Food') }}</title>
 
   <link rel="icon" type="image/png" href="{{asset('images/logo.png')}}" sizes="196x196" />
   <!-- Fonts -->
   <link rel="dns-prefetch" href="//fonts.gstatic.com">
-  <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.2/css/bootstrap.min.css" integrity="sha384-y3tfxAZXuh4HwSYylfB+J125MxIs6mR5FOHamPBG064zB+AFeWH94NdvaCBm8qnd" crossorigin="anonymous">
   
   <!-- Styles -->
